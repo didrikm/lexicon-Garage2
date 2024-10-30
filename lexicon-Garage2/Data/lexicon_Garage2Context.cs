eb7887fc6fe9a515c1f12bd3b9b5f77cd0d36d94
didrikm <didrik.molinder@protonmail.com>
2024-10-30 13:43:30 +0100

    Added registration constraint to DbContext

--- lexicon-Garage2/Data/lexicon_Garage2Context.cs
+++ lexicon-Garage2/Data/lexicon_Garage2Context.cs
@@ -9,11 +9,20 @@ namespace lexicon_Garage2.Data
 {
     public class lexicon_Garage2Context : DbContext
     {
-        public lexicon_Garage2Context (DbContextOptions<lexicon_Garage2Context> options)
+        public lexicon_Garage2Context(DbContextOptions<lexicon_Garage2Context> options)
             : base(options)
         {
         }
 
         public DbSet<lexicon_Garage2.Models.Vehicle> Vehicle { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Vehicle>()
+                .HasIndex(v => v.RegistrationNumber)
+                .IsUnique();
+        }
     }
 }

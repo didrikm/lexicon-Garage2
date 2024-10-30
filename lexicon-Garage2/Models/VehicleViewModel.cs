c9a0ae67fec7a39f68e05e3dceda4f1e7ad94c88
Luna <hanken95@gmail.com>
2024-10-30 11:24:12 +0100

    VehicleViewModel Created

---
+++ lexicon-Garage2/Models/VehicleViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace lexicon_Garage2.Models
+{
+    public class VehicleViewModel
+    {
+        public int Id { get; set; }
+        public string VehicleType { get; set; }
+        public string RegistrationNumber { get; set; }
+        public DateTime ArrivalTime { get; set; }
+    }
+}

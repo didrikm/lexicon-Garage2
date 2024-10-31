088a70794312954c793dec57147ec8824b865f1b
didrikm <didrik.molinder@protonmail.com>
2024-10-31 09:46:46 +0100

    Added ReceiptViewModel

---
+++ lexicon-Garage2/Models/ReceiptViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace lexicon_Garage2.Models
+{
+    public class ReceiptViewModel
+    {
+        public string RegistrationNumber { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime CheckOutTime { get; set; }
+        public float DurationOfParking { get; set; }
+        public float Total { get; set; }
+        public ReceiptViewModel(Vehicle vehicle)
+        {
+            RegistrationNumber = vehicle.RegistrationNumber;
+            CheckInTime = vehicle.ParkingTime;
+            CheckOutTime = DateTime.Now;
+            DurationOfParking = (float)(CheckOutTime - CheckInTime).TotalHours;
+            Total = 100 * DurationOfParking;
+        }
+    }
+}
